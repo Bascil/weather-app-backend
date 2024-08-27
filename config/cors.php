7a90a4a5eed4925c5720a137155b3b449519adc1
Basil Ndonga <basilndonga@gmail.com>
2024-08-27 23:27:51 +0300

    updated cors configuration

--- config/cors.php
+++ config/cors.php
@@ -13,13 +13,13 @@ return [
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
-    */
+     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://weather-app-frontend-ui.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
