f6daad1b9701c94406751fdc51745bdd691cacf3
Basil Ndonga <basilndonga@gmail.com>
2024-08-28 10:53:27 +0300

    added mocks to tests

--- tests/Feature/WeatherApiTest.php
+++ tests/Feature/WeatherApiTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Services\WeatherApiService;
 use Illuminate\Http\Response;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class WeatherApiTest extends TestCase
@@ -10,6 +12,15 @@ class WeatherApiTest extends TestCase
     public function test_can_get_weather_data_with_city()
     {
         $headers = [];
+
+        $this->instance(
+            WeatherApiService::class,
+            \Mockery::mock(WeatherApiService::class, function (MockInterface $mock) {
+                $mock->shouldReceive('getWeatherData')
+                    ->once();
+            })
+        );
+
         $response = $this->get('/api/v1/weather/data?city=kericho&units=metric', $headers);
         $response->assertStatus(Response::HTTP_OK);
     }
@@ -17,6 +28,15 @@ class WeatherApiTest extends TestCase
     public function test_can_get_weather_data_without_city()
     {
         $headers = [];
+
+        $this->instance(
+            WeatherApiService::class,
+            \Mockery::mock(WeatherApiService::class, function (MockInterface $mock) {
+                $mock->shouldReceive('getWeatherData')
+                    ->once();
+            })
+        );
+
         $response = $this->get('/api/v1/weather/data?units=metric', $headers);
         $response->assertStatus(Response::HTTP_OK);
     }
@@ -24,6 +44,15 @@ class WeatherApiTest extends TestCase
     public function test_can_get_weather_forecast()
     {
         $headers = [];
+
+        $this->instance(
+            WeatherApiService::class,
+            \Mockery::mock(WeatherApiService::class, function (MockInterface $mock) {
+                $mock->shouldReceive('getWeatherForecast')
+                    ->once();
+            })
+        );
+
         $response = $this->get('/api/v1/weather/forecast?cnt=24&units=metric', $headers);
         $response->assertStatus(Response::HTTP_OK);
     }
